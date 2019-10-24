40ef1ba124eecb8e964d0c1f34023367f408a7ac
Lauro Soares <lauro_bs8@hotmail.com>
2019-10-24 12:26:22 -0300

    fix array

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,15 @@ const fs = require("fs");
 const cors = require('cors')
 const testFolder = "C:/Users/lbezerrs/Documents";
 
-
 const dados = []
 
+app.use(cors())
+
 fs.readdir(testFolder, (err, files) => {
-  files.forEach(file => dados.push(file));
+  files.forEach(file => {
+    Array.from(file)
+    dados.push(file);
+  })
   console.log(dados);
 });
 const rota = express.Router()
@@ -17,13 +21,12 @@ const rota = express.Router()
 app.get('/', (req, res) => {
   res.send(`${dados}`)
 })
-
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`)
-  console.log('Para derrubar o servidor: ctrl + c');
+
+console.log(`Servidor rodando em http://localhost:${port}`)
+console.log('Para derrubar o servidor: ctrl + c');
 })
 
-app.use(cors())
 app.use(express.json())
 
 module.exports = rota
